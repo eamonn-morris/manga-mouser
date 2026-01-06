8fedc7c647ca33ebdb62a7a9891ae61f3c30fb77
eamonn <eamonnmorris@protonmail.com>
2026-01-05 18:48:26 -0600

    added function to return rss feed entries in abbreviated format

--- main.py
+++ main.py
@@ -10,13 +10,34 @@ load_dotenv()
 user_agent = "MangaMouser/1.0"
 FEED_URL = os.getenv("FEED_URL")
 
-manga_feed = feedparser.parse(FEED_URL, agent=user_agent)
+
+# manga_feed = feedparser.parse(FEED_URL, agent=user_agent)
+def get_feed_entries(rss_url):
+    """
+    returns dictionary of rss feed entries
+    """
+    manga_feed = feedparser.parse(rss_url, agent=user_agent)
+    feed_entries = manga_feed.entries
+
+    entry_list = []
+
+    for entry in feed_entries:
+        temp = dict()
+
+        temp["title"] = entry.title
+        temp["link"] = entry.link
+        temp["category"] = entry.nyaa_category
+
+        entry_list.append(temp)
+
+    return entry_list
 
 
 def main():
-    print(manga_feed.feed.title)  # pyright: ignore[reportAttributeAccessIssue]
     print("Hello from MangaMouser!")
-    print(json.dumps(manga_feed.entries, indent=2))
+    feed_entries = get_feed_entries(rss_url=FEED_URL)
+    print(json.dumps(feed_entries, indent=2))
+    print(f"New Entries Found: {len(feed_entries)}")
 
 
 if __name__ == "__main__":
